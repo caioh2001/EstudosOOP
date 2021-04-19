e9aa9480471b1d327b8cf9bb5ac1805faa681846
Caio Henrique <caio.henrique.20111@gmail.com>
2021-04-19 09:55:43 -0300

    Mudanca nome solucao.

---
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CursoCSharp.OO;
+using CursoCSharp.MetodosEFuncoes;
+using CursoCSharp.Excecoes;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encapsulamentos en = new Encapsulamentos();
+            en.Executar();
+
+            Polimorfismo poli = new Polimorfismo();
+            poli.Executar();
+
+            Abstract ab = new Abstract();
+            ab.Executar();
+
+            Interface inter = new Interface();
+            inter.Executar();
+
+            Sealed sea = new Sealed();
+            sea.Executar();
+
+            ExemploLambda ex = new ExemploLambda();
+            ex.Executar();
+
+            LambdasDelegate lamb = new LambdasDelegate();
+            lamb.Executar();
+
+            UsandoDelegates use = new UsandoDelegates();
+            use.Executar();
+
+            DelegateFunAnonima func = new DelegateFunAnonima();
+            func.Executar();
+
+            DelegatesComoParametros del = new DelegatesComoParametros();
+            del.Executar();
+
+            MetodosDeExtencao met = new MetodosDeExtencao();
+            met.Executar();
+
+            PrimeiraExcecao exc = new PrimeiraExcecao();
+            exc.Executar();
+
+            ExcecoesPersonalizadas pers = new ExcecoesPersonalizadas();
+            pers.Executar();
+
+            Console.ReadKey();
+        }
+    }
+}
