ffad57bec145a505e7fcd05454d31e0bbef23ff8
Caio Henrique <caio.henrique.20111@gmail.com>
2021-04-22 12:37:51 -0300

    Enumeracoes.

---
+++ CursoCSharp/CursoCSharp/ClassesEMetodos/ExemploEnum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public enum Genero { Acao, Aventura, Terror, Animacao, Comedia };
+
+    public class Filme
+    {
+        public string Titulo;
+        public Genero GeneroFilme;
+    }
+
+    class ExemploEnum
+    {
+        public static void Executar()
+        {
+            int id = (int)Genero.Animacao;
+            Console.WriteLine(id);
+
+            var filmeParaFamilia = new Filme();
+            filmeParaFamilia.Titulo = "Sharknado 15";
+            filmeParaFamilia.GeneroFilme = Genero.Comedia;
+
+            Console.WriteLine($"{filmeParaFamilia.Titulo} e " +
+                $"{filmeParaFamilia.GeneroFilme}!");
+        } 
+    }
+}
