516488f4d816be1b67859fc3b211066552c3dda9
Caio Henrique <caio.henrique.20111@gmail.com>
2021-04-24 15:38:46 -0300

    Usando path.

---
+++ CursoCSharp/CursoCSharp/API/ExemploDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.API
+{
+    class ExemploDateTime
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
