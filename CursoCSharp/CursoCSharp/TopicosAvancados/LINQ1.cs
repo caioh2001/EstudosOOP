62899323f395dda46719a3dfd4740250848f0bb2
Caio Henrique <caio.henrique.20111@gmail.com>
2021-04-25 11:38:42 -0300

    LINQ.

---
+++ CursoCSharp/CursoCSharp/TopicosAvancados/LINQ1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CursoCSharp.TopicosAvancados
+{
+    public class Aluno
+    {
+        public string Nome;
+        public int Idade;
+        public double Nota;
+
+        public Aluno(string nome, int idade, double nota)
+        {
+            Nome = nome;
+            Idade = idade;
+            Nota = nota;
+        }
+    }
+
+    class LINQ1
+    {
+        public static void Executar()
+        {
+            var alunos = new List<Aluno>
+            {
+                new Aluno("Pedro", 24, 8.0),
+                new Aluno("Andre", 21, 4.3),
+                new Aluno("Ana", 25, 9.5),
+                new Aluno("Jorge", 20, 8.5),
+                new Aluno("Ana", 21, 7.7),
+                new Aluno("Julia", 22, 7.5),
+                new Aluno("Marcio", 18, 6.8)
+            };
+
+            Console.WriteLine("=== Aprovados ===");
+            var aprovados = alunos.Where(a => a.Nota >= 7).OrderBy(a => a.Nome);
+            foreach(var aluno in aprovados)
+            {
+                Console.WriteLine(aluno.Nome);
+            }
+
+            Console.WriteLine("=== Chamada ===");
+            var chamada = alunos.OrderBy(a => a.Nome).Select(a => a.Nome);
+            foreach(var aluno in chamada)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            Console.WriteLine("=== Aprovados (por idade) ===");
+            var alunosAprovados = from aluno in alunos
+                                  where aluno.Nota >= 7
+                                  orderby aluno.Idade
+                                  select aluno.Nome;
+            
+            foreach(var aluno in alunosAprovados)
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+    }
+}
---
+++ CursoCSharp/CursoCSharp/TopicosAvancados/LINQ2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CursoCSharp.TopicosAvancados
+{
+    class LINQ2
+    {
+        public static void Executar()
+        {
+            var alunos = new List<Aluno>
+            {
+                new Aluno("Pedro", 24, 8.0),
+                new Aluno("Andre", 21, 4.3),
+                new Aluno("Ana", 25, 9.5),
+                new Aluno("Jorge", 20, 8.5),
+                new Aluno("Ana", 21, 7.7),
+                new Aluno("Julia", 22, 7.5),
+                new Aluno("Marcio", 18, 6.8)
+            };
+
+            var pedro = alunos.Single(aluno => aluno.Nome.Equals("Pedro"));
+            Console.WriteLine($"{pedro.Nome} {pedro.Nota}");
+
+            var fulano = alunos.SingleOrDefault(aluno => aluno.Nome.Equals
+            ("Fulano"));
+            if (fulano == null)
+            {
+                Console.WriteLine("Aluno inexistente.");
+            }
+
+            var ana = alunos.First(aluno => aluno.Nome.Equals("Ana"));
+            Console.WriteLine(ana.Nota);
+
+            var sicrano = alunos.FirstOrDefault(aluno => aluno.Nome
+            .Equals("Sicrano"));
+            if (sicrano == null)
+            {
+                Console.WriteLine("Aluno inexistente.");
+            }
+
+            var outraAna = alunos.LastOrDefault(aluno => aluno.Nome
+            .Equals("Ana"));
+            Console.WriteLine(outraAna.Nota);
+
+            var exemploSkip = alunos.Skip(1).Take(3);
+            foreach (var aluno in exemploSkip)
+            {
+                Console.WriteLine(aluno.Nome);
+            }
+
+            var maiorNota = alunos.Max(aluno => aluno.Nota);
+            Console.WriteLine(maiorNota);
+
+            var menorNota = alunos.Min(aluno => aluno.Nota);
+            Console.WriteLine(menorNota);
+
+            var somatorioNotas = alunos.Sum(aluno => aluno.Nota);
+            Console.WriteLine(somatorioNotas);
+
+            var mediaDaTurma = alunos.Where(a => a.Nota >= 7)
+                .Average(aluno => aluno.Nota);
+            Console.WriteLine(mediaDaTurma);
+        }
+    }
+}
